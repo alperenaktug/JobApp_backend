6868fee0ff83e69b189b50ab4a7a20dc3e662770
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-09-19 12:52:35 +0300

    Handling Form

--- src/main/java/com/starter/JobApp/JobController.java
+++ src/main/java/com/starter/JobApp/JobController.java
@@ -1,20 +1,29 @@
 package com.starter.JobApp;
 
+import com.starter.JobApp.model.JobPost;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
 
-    @RequestMapping({"/" , "home"})
+    @GetMapping({"/" , "home"})
     public String home()
     {
         return "home" ;
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addJob()
     {
         return "addjob" ;
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost)
+    {
+        return "success" ;
     }
 }
