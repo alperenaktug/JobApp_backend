3c6485d0ceaeeb938540682d8f266c62d088be44
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-09-19 13:00:11 +0300

    Layers and View Data

---
+++ src/main/java/com/starter/JobApp/service/JobService.java
@@ -0,0 +1,27 @@
+package com.starter.JobApp.service;
+
+import com.starter.JobApp.model.JobPost;
+import com.starter.JobApp.repository.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo ;
+
+
+    public void addJob( JobPost jobPost)
+    {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs()
+    {
+        return repo.getAllJobs() ;
+    }
+
+}
