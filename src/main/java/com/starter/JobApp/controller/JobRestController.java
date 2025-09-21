c0c0a146eecac8a6eaa0322449ceb19f0548cc2c
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-09-21 14:40:24 +0300

    jpa in JobAPP AND Load data and Entity

--- src/main/java/com/starter/JobApp/controller/JobRestController.java
+++ src/main/java/com/starter/JobApp/controller/JobRestController.java
@@ -46,5 +46,11 @@ public class JobRestController {
         return "Deleted" ;
     }
 
+    @GetMapping("load")
+    public String loadData(){
+        service.load();
+        return "success" ;
+    }
+
 
 }
--- src/main/java/com/starter/JobApp/service/JobService.java
+++ src/main/java/com/starter/JobApp/service/JobService.java
@@ -5,6 +5,7 @@ import com.starter.JobApp.repository.JobRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,31 +14,51 @@ public class JobService {
     @Autowired
     private JobRepo repo ;
 
-
     public void addJob(JobPost jobPost) {
-        repo.addJob(jobPost);
-
+        repo.save(jobPost);
     }
 
-    public List<JobPost> getAllJobs()
-    {
-        return repo.getAllJobs() ;
+    public List<JobPost> getAllJobs() {
+        return repo.findAll();
     }
-
-
 
     public JobPost getJob(int postId) {
-
-        return repo.getJob(postId);
+        return repo.findById(postId).orElse(new JobPost());
     }
 
     public void updateJob(JobPost jobPost) {
-
-        repo.updateJob(jobPost);
-
+        repo.save(jobPost);
     }
 
     public void deleteJob(int postId) {
-        repo.deleteJob(postId);
+        repo.deleteById(postId);
+    }
+
+    public void load() {
+        List<JobPost> jobs =
+                new ArrayList<>();
+
+        // Java Developer Job Post
+        jobs.add(new JobPost(1, "Java Developer", "Must have good experience in core Java and advanced Java", 2,
+                List.of("Core Java", "J2EE", "Spring Boot", "Hibernate")));
+
+        // Frontend Developer Job Post
+        jobs.add(new JobPost(2, "Frontend Developer", "Experience in building responsive web applications using React",
+                3, List.of("HTML", "CSS", "JavaScript", "React")));
+
+        // Data Scientist Job Post
+        jobs.add(new JobPost(3, "Data Scientist", "Strong background in machine learning and data analysis", 4,
+                List.of("Python", "Machine Learning", "Data Analysis")));
+
+        // Network Engineer Job Post
+        jobs.add(new JobPost(4, "Network Engineer",
+                "Design and implement computer networks for efficient data communication", 5,
+                List.of("Networking", "Cisco", "Routing", "Switching")));
+
+        // Mobile App Developer Job Post
+        jobs.add(new JobPost(5, "Mobile App Developer", "Experience in mobile app development for iOS and Android", 3,
+                List.of("iOS Development", "Android Development", "Mobile App")));
+
+        repo.saveAll(jobs);
     }
 }
