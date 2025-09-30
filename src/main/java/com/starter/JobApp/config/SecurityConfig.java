3f17e2872beb229100eb12f16dd598bbfd484c17
Alperen Aktuğ <aktugalperen9@gmail.com>
2025-09-30 17:43:03 +0300

    securing config

--- src/main/java/com/starter/JobApp/config/SecurityConfig.java
+++ src/main/java/com/starter/JobApp/config/SecurityConfig.java
@@ -11,11 +11,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
     private UserDetailsService userDetailsService;
